ec608e18c987669015e7cb9628f13f083e291753
Pablo <pablolp257@gmail.com>
2026-03-23 23:23:01 +0100

    🚀 Portfolio 3D (2º DAM)

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './style.css';
+import '@splinetool/viewer';
+
+// Toda la estructura visual interactiva de tu portfolio está 
+// soportada a pantalla completa por tu Spline Export nativo.
+//
+// Los enlaces para Contacto ("Hablemos" y "Email") ahora son 
+// elementos DOM nativos colocados a mano por encima del canvas de WebGL,
+// solucionando al 100% cualquier problema con el Pop-Up blocker.
